2588fc665784aad947252933875ea5243ceb60e4
Naidich <mnaidich@alixpartners.com>
2025-11-07 12:50:38 -0300

    test15

--- modules/ec2/outputs.tf
+++ modules/ec2/outputs.tf
@@ -12,3 +12,8 @@ output "role_name" {
   description = "Name of the IAM role assigned to the instance"
   value       = aws_iam_role.role_streamlit_app.name
 }
+
+output "public_dns" {
+  description = "Public DNS name of the EC2 instance"
+  value       = aws_instance.streamlit_host.public_dns
+}
