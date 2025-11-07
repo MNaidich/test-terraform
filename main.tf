2a34a61de9e6b16354100b6a8397c38c4abf8003
Naidich <mnaidich@alixpartners.com>
2025-11-07 13:46:26 -0300

    test22

--- main.tf
+++ main.tf
@@ -1,6 +1,11 @@
 # Apply default tags to all AWS resources
 provider "aws" {
-  region  = var.aws_region
+  access_key = "fake"
+  secret_key = "fake"
+  region     = "us-east-1"
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
 
   default_tags {
     tags = var.default_tags
